58685fd4ae03e48b166a8da410464d09942fd7cd
Rafael T. C. Soares <rafaelcba@gmail.com>
2019-10-01 19:53:49 -0300

    using Fuse 7.4 BOM; exposing API Gateway as an endpoint

--- camel-saga-app/src/main/java/me/nicolaferraro/quickstarts/saga/CamelSagaApp.java
+++ camel-saga-app/src/main/java/me/nicolaferraro/quickstarts/saga/CamelSagaApp.java
@@ -3,6 +3,7 @@ package me.nicolaferraro.quickstarts.saga;
 import org.apache.camel.Exchange;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.model.SagaPropagation;
+import org.apache.camel.model.rest.RestBindingMode;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Component;
@@ -19,13 +20,31 @@ public class CamelSagaApp {
         @Override
         public void configure() throws Exception {
 
-            from("timer:clock?period=5s")
-                    .saga()
-                        .setHeader("id", header(Exchange.TIMER_COUNTER))
-                        .setHeader(Exchange.HTTP_METHOD, constant("POST"))
-                        .log("Executing saga #${header.id}")
-                        .to("http4://camel-saga-train-service:8080/api/train/buy/seat")
-                        .to("http4://camel-saga-flight-service:8080/api/flight/buy");
+            // from("timer:clock?period=5s")
+            restConfiguration()
+                .apiContextPath("/api-doc")
+                .apiProperty("api.title", "My Sagas REST API")
+                .apiProperty("api.version", "1.0")
+                .apiProperty("cors", "true")
+                .apiProperty("base.path", "/api")
+                .apiProperty("api.path", "/")
+                .apiProperty("host", "")
+                .apiContextRouteId("doc-api")
+                    .component("servlet")
+                    .dataFormatProperty("json.in.disableFeatures", "FAIL_ON_EMPTY_BEANS,FAIL_ON_UNKNOWN_PROPERTIES")
+                    .bindingMode(RestBindingMode.auto);
+            
+            rest()
+                .post("/saga/start").description("Initiate a new SAGA request...")
+                .route().routeId("reservation")
+                .log("Make a new reservation.")
+                .saga()
+                    .setHeader("id", header(Exchange.TIMER_COUNTER))
+                    .setHeader(Exchange.HTTP_METHOD, constant("POST"))
+                    .log("Executing saga #${header.id}")
+                    .to("http4://camel-saga-train-service:8080/api/train/buy/seat?bridgeEndpoint=true")
+                    .to("http4://camel-saga-flight-service:8080/api/flight/buy?bridgeEndpoint=true")
+            .endRest();
 
         }
     }
